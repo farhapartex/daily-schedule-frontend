b33e5ac79995ebfa9255559ef5b50ff6edc90a76
Md Nazmul Hasan <farhapartex@gmail.com>
2021-12-05 22:55:36 +0600

    Dashboard component added

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = ()=> {
+    return (
+        <div className="w-100 p-3" id="dashboard">
+            <div className="pl-5 mt-3">
+                <h3>Welcome to Schedule, Mr. Nazmul Hasan</h3>
+                <h4 className="mt-4">Today | 05 December 2021</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Routes, Route,} from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 const RightSide = ()=> {
     return (
-        <div className="col-10" id="rightSide"></div>
+        <div className="col-10" id="rightSide">
+            <Routes>
+                <Route path="/" element={<Dashboard />}/>
+            </Routes>
+        </div>
     )
 }
 
