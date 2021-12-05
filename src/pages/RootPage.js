7a8b1fcf991a91ef406def40453f530ea040c4b3
Md Nazmul Hasan <farhapartex@gmail.com>
2021-12-05 22:15:48 +0600

    Leftsidebar added in RootPage

--- src/pages/RootPage.js
+++ src/pages/RootPage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "../static/css/RootPage.css";
 
 const RootPage = ()=> {
     return (
-        <div>
-            Root Page
+        <div id="rootPage">
+            <Container fluid>
+                <div className="row">
+                    <div className="col-2" id="leftSide">
+                        <div id="brandName" className="pt-4 pb-3">
+                            <h4 className="text-center">Schedule</h4>
+                        </div>
+
+                        <div id="sidebarMenu">
+                            <ul>
+                                <li><a href="/">Dashboard</a></li>
+                                <li><a href="/">Schedule</a></li>
+                                <li><a href="/">Profile</a></li>
+                                <li><a href="/">Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-10" id="rightSide"></div>
+                </div>
+            </Container>
         </div>
     )
 }
