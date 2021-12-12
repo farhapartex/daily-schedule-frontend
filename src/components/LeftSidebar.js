4ce3ee05ed61f511fc08174577cef1e1f77d2ec4
Md Nazmul Hasan <farhapartex@gmail.com>
2021-12-12 15:24:21 +0600

    refactor css in sidebarmenu

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,7 +5,7 @@ const LeftSidebar = ()=> {
     return (
         <div className="col-2" id="leftSide">
             <div id="brandName" className="pt-4 pb-3">
-                <h4 className="text-center">Schedule</h4>
+                <h4 className="text-center">Daily Schedule</h4>
             </div>
 
             <SidebarMenu />
